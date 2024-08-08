0c834fbac4af4b76afe2fe8801161d951a70d139
riedhampattel <riedhampattel.tops@gmail.com>
2024-08-08 17:11:21 +0530

    Add files via upload

---
+++ CPP/Basics/3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 10;
+	double p = 45.69;
+	char ch = 'A';
+	string msg= "Hello";
+	bool y = false;
+	
+	cout<<"\nValue of integer = "<<x;
+	cout<<"\nValue of double = "<<p;
+	cout<<"\nValue of character = "<<ch;
+	cout<<"\nValue of string = "<<msg;
+	cout<<"\nValue of boolean = "<<y;
+	
+	return 0;
+}
---
+++ CPP/Basics/4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	double p;
+	char ch;
+	string msg;
+	
+	//input in cpp
+	cout<<"\nEnter the value of integer x = ";
+	cin>>x;
+	cout<<"\nEnter the value of double p = ";
+	cin>>p;
+	cout<<"\nEnter the value of char ch = ";
+	cin>>ch;
+	cout<<"\nEnter the value of string msg = ";
+	cin>>msg;
+	
+	//output in cpp
+	cout<<"\nValue of integer = "<<x;
+	cout<<"\nValue of double = "<<p;
+	cout<<"\nValue of character = "<<ch;
+	cout<<"\nValue of string = "<<msg;
+	
+	return 0;
+}
