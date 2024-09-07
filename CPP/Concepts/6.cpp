e1dd8329f895cbb8a90eb1b555588ed08a41d8d1
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-07 17:04:18 +0530

    Add files via upload

---
+++ CPP/Concepts/6.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+//shallow copy
+class Demo
+{
+	public :
+		double *cgpaptr;
+		string name;
+		Demo(string name,double cgpa)
+		{
+			this->name = name;
+			cgpaptr = new double;
+			*cgpaptr = cgpa;
+		}
+//		Demo(Demo &obj)
+//		{
+//			name = obj.name;
+//			cgpa = obj.cgpa;
+//		}
+		void display()
+		{
+			cout<<endl;
+			cout<<"\nName = "<<name;
+			cout<<"\ncgpa = "<<*cgpaptr;
+		}
+};
+int main()
+{
+	Demo d1("Rahul",8.5);
+	Demo d2(d1);
+	
+	cout<<"Before value change = ";
+	d1.display();
+	d2.display();
+	
+	*d2.cgpaptr = 7.2;
+	
+	cout<<"\nAfter value change = ";
+	d1.display();
+	d2.display();
+	return 0;	
+}
